6cbef62236e4480eee99778719be2cda50343880
Matt Dobson <mdobson4@gmail.com>
2022-12-11 12:54:11 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  talkShop() {
+    console.log(`This ones a beaut. Great example of a ${make} ${model}`);
+  }
+}
