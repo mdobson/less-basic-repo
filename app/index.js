980b474455036f9601e16d25435a65343f45ac33
Matt Dobson <mdobson4@gmail.com>
2022-12-11 12:03:53 -0500

    Fourth commit

--- app/index.js
+++ app/index.js
@@ -5,3 +5,16 @@ function helloer(nameOfPerson) {
 }
 
 helloer(personsName);
+
+class Person {
+  constructor(personsName) {
+    this.name = personsName;
+  }
+
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+}
+
+const b = new Person("Matt");
+b.greet();
